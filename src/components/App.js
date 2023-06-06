816a2e434b0d77eeff56777390b754b9c8f92050
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-06 15:25:28 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  return (
+    <div id="main">
+      <h2>Task Counter</h2>
+      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
+      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
+    </div>
+  )
+}
+
+
+export default App;
